b7246939d72ae9298fcf71bac8b001d95ac09db4
CamCufre <cufrecamila1@gmail.com>
2023-06-23 10:29:04 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,35 @@
+// const { Router } = require('express');
+// // Importar todos los routers;
+// // Ejemplo: const authRouter = require('./auth.js');
+
+// const dietsRouter = require('../routes/dietsRouter')
+// const recipesRouter = require('../routes/recipesRouter')
+// const router = Router();
+
+// // Configurar los routers
+// // Ejemplo: router.use('/auth', authRouter);
+
+// router.use('/diets', dietsRouter)
+// router.use('/recipes', recipesRouter)
+
+// module.exports = router;
+
 const { Router } = require('express');
+const cors = require('cors');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dietsRouter = require('../routes/dietsRouter');
+const recipesRouter = require('../routes/recipesRouter');
 
 const router = Router();
 
+// Configurar CORS
+router.use(cors());
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/diets', dietsRouter);
+router.use('/recipes', recipesRouter);
 
 module.exports = router;
