104fd113557cd28580c560b39b3109e2752c71bf
CamCufre <cufrecamila1@gmail.com>
2023-06-23 10:27:20 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,8 +21,10 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+// conn.sync({ force: true }).then(() => { //force es para romper todo, para hacer grandes cambios en la db
+conn.sync({ alter: true }).then(() => { //con esta wea no c borran los datos de la db, pa deployear en false
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('listening at 3001... a ksa pvto >:)'); // eslint-disable-line no-console
   });
 });
+
