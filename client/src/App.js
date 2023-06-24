384b98ae4c69eaebcfa1a91a4a7ae9c777566d2e
CamCufre <cufrecamila1@gmail.com>
2023-06-24 18:36:33 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/landing/Landing';
+import Home from './components/home/Home'
+import Form from './components/createRecipe/Form';
+import Details from './components/details/Details';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home currentPage={currentPage} setCurrentPage={setCurrentPage} />} />
+          <Route path="/createRecipe" element={<Form />} />
+          <Route path="/recipeDetails/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
