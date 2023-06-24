c8c515ecf77e35b35f9622900613947ea5ceb682
CamCufre <cufrecamila1@gmail.com>
2023-06-24 18:39:09 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from "./components/redux/store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
+//Desde la version de react 18 la definicion de la raiz del proyecto se define asi, en vez de ReactDOM.render:
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
