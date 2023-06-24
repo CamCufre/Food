fb09262c5166bbeb823f8cfbb9c2832c16e0b87b
CamCufre <cufrecamila1@gmail.com>
2023-06-24 18:37:35 -0300

    Create BoxesContainer.jsx

---
+++ client/src/components/BoxesContainer/BoxesContainer.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { connect } from 'react-redux';
+import { getAllRecipes } from '../redux/actions';
+import RecipeBox from "../recipebox/RecipeBox";
+import Pagination from "../pagination/Pagination";
+import style from './BoxesContainer.module.css'
+
+const BoxesContainer = ({ recipes, getAllRecipes, currentPage, setCurrentPage }) => {
+  useEffect(() => {
+    getAllRecipes();
+  }, []);
+
+  const recipesPerPage = 15;
+  const totalPages = Math.ceil(recipes.length / recipesPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const recipeBoxes = recipes
+    .slice((currentPage - 1) * recipesPerPage, currentPage * recipesPerPage)
+    .map(({ id, name, image, healthscore, diets }) => (
+      <RecipeBox
+        key={id}
+        id={id}
+        name={name}
+        image={image}
+        healthscore={healthscore}
+        diets={diets}
+      />
+    ));
+
+  return (
+    <div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePrevPage={handlePrevPage}
+        handlePageChange={handlePageChange}
+        handleNextPage={handleNextPage}
+      />   
+      {recipes.length === 0 ? (
+        <div className={style.error}>No recipes found.</div>
+      ) : (
+        <div className={style.container}>
+          {recipeBoxes}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.allHomeRecipes
+  };
+};
+
+const mapDispatchToProps = {
+  getAllRecipes
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoxesContainer);
+
