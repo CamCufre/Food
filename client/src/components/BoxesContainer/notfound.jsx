b69c1aafd46fe84a911180ec8a8a3044e72a798e
CamCufre <cufrecamila1@gmail.com>
2023-06-28 00:51:29 -0300

    Create notfound.jsx

---
+++ client/src/components/BoxesContainer/notfound.jsx
@@ -0,0 +1,10 @@
+import style from './notfound.module.css'
+
+export const NotFound = () => {
+
+    return (
+        <>
+        <div className={style.errorContainer}>No recipes found.</div>
+        </>
+    )
+}
